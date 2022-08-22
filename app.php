d49edeee87e9e8955246948c8497f9aac7599cc2
SerafimArts <nesk@xakep.ru>
2022-08-22 14:35:46 +0300

    Remove vendor deps

--- app.php
+++ app.php
@@ -1,5 +1,12 @@
 <?php
+
+declare(strict_types=1);
+
 use App\Game;
+
+if (!is_file(__DIR__ . '/vendor/autoload.php')) {
+    throw new \LogicException();
+}
 
 require __DIR__ . '/vendor/autoload.php';
 
